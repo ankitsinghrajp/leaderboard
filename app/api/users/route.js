6ab7bc9f629a0575192f25d4e06dd0f06a1864da
ankit singh chouhan <ankitsinghchouhan682@gmail.com>
2025-07-15 16:47:24 +0530

    User fetching functionality at frontend successfull

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(){
     await connectDb();
 
-    const users = await User.find().sort({points:-1});
-    return NextResponse.json(users);
+    const data = await User.find().sort({points:-1});
+    return NextResponse.json(data);
 }
+
--- components/select-user-card.jsx
+++ components/select-user-card.jsx
@@ -1,4 +1,7 @@
+"use client"
 import React from "react";
+import { useState,useEffect } from "react";
+
 import {
   Card,
   CardContent,
@@ -11,6 +14,34 @@ import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 
 const SelectUserCard = () => {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(()=>{
+
+   
+    const fetchUsers = async ()=>{
+
+        try {
+            const res = await fetch("/api/users");
+            if(!res.ok) throw new Error("Failed to fetch users");
+            const data = await res.json();
+            setUsers(data);
+        } catch (error) {
+            console.error("Error loading users: ",error);
+        } 
+        finally{
+            setLoading(false);
+        }
+
+     };
+
+     fetchUsers();
+
+    },[]);
+
   return (
     <Card className={'h-[500px] border-1 bg-[#09090b] border-gray-700 shadow-md shadow-green-500/10'}>
       <CardHeader>
@@ -21,26 +52,34 @@ const SelectUserCard = () => {
             </h2>
         </CardTitle>          
       </CardHeader>
+      {loading?<div className=" text-center h-[400px] p-4 text-emerald-600 font-semibold">
+          Loding Users...
+      </div>:
       <CardContent className={'h-[400px]  overflow-y-scroll custom-scrollbar'}>
-        <ul>
-            <li className="flex justify-between items-center border border-emerald-500/40 shadow-md shadow-emerald-500/10 px-3 py-3 rounded-md">
+        <ul className="py-5">
+            {users.map((user)=>{
+
+            return <li className="flex my-4 justify-between items-center border border-emerald-500/40 shadow-md shadow-emerald-500/10 px-3 py-3 rounded-md">
                 <div className="flex items-center gap-2">
                     <div className="bg-emerald-500/80 p-2 rounded-full">
                        <User/>
                     </div>
                     <div className="flex flex-col">
-                       <span className="text-sm font-medium">Sushant Singh</span>
+                       <span className="text-sm font-medium">{user.name}</span>
                        <span className="text-sm text-muted-foreground">Rank: #1</span>
                     </div>
                 </div>
                 <div className="">
                     <Badge variant={'outline'}>
-                        85 pts
+                        {user.totalPoints} pts
                     </Badge>
                 </div>
             </li>
+              })}
         </ul>
       </CardContent>
+}
+
       <CardFooter>
         <Button variant={''} className={'w-full shadow-md text-white border border-gray-600 bg-[#09090b] shadow-black/20 cursor-pointer hover:bg-black/20'}>
             <Plus className="w-10 h-10"/>
